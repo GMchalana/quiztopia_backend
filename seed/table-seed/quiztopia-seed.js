e6eee424b03fe41d379b8d059c19d7bb893f723e
GMchalana <gmchalanaprabhashwara@gmail.com>
2025-04-10 00:47:30 +0530

    Add database seeding for module, MC questions, MCQ answers, and TF questions tables

--- seed/table-seed/quiztopia-seed.js
+++ seed/table-seed/quiztopia-seed.js
@@ -1,4 +1,8 @@
 const { createUsersTable } = require('../tables/quiztopia');
+const { createModuleTable } = require('../tables/quiztopia');
+const { createMcQuestionsTable } = require('../tables/quiztopia');
+const { createMcqAnswersTable } = require('../tables/quiztopia');
+const { createTfQuestionsTable } = require('../tables/quiztopia');
 
 
 
@@ -6,6 +10,19 @@ const seedQuiztopia = async () => {
     try {
       const messagecreateUsersTable = await createUsersTable();
       console.log(messagecreateUsersTable);
+
+      const messagecreateModuleTable= await createModuleTable();
+      console.log(messagecreateModuleTable);
+  
+      const messagecreateMcQuestionsTable = await createMcQuestionsTable();
+      console.log(messagecreateMcQuestionsTable);
+  
+      const messagecreateMcqAnswersTable = await createMcqAnswersTable();
+      console.log(messagecreateMcqAnswersTable);
+  
+      const messagecreateTfQuestionsTable = await createTfQuestionsTable();
+      console.log(messagecreateTfQuestionsTable);
+  
   
       
       
--- seed/tables/quiztopia.js
+++ seed/tables/quiztopia.js
@@ -12,7 +12,7 @@ const createUsersTable = () => {
       email VARCHAR(150) DEFAULT NULL,
       userName VARCHAR(50) DEFAULT NULL,
       role VARCHAR(25) DEFAULT NULL,
-      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
   `;
     return new Promise((resolve, reject) => {
@@ -28,7 +28,115 @@ const createUsersTable = () => {
 
 
 
+const createModuleTable = () => {
+    const sql = `
+    CREATE TABLE IF NOT EXISTS module (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      moduleName VARCHAR(50) DEFAULT NULL,
+      numOfQuestions INT(5) DEFAULT NULL,
+      estimationTime DECIMAL(15, 2) DEFAULT NULL,
+      deleteStatus BOOLEAN DEFAULT NULL,
+      createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )
+  `;
+    return new Promise((resolve, reject) => {
+        db.query(sql, (err, result) => {
+            if (err) {
+                reject('Error creating module table: ' + err);
+            } else {
+                resolve('module table created successfully. ✅');
+            }
+        });
+    });
+};
+
+
+const createMcQuestionsTable = () => {
+    const sql = `
+    CREATE TABLE IF NOT EXISTS mcquestions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    moduleId INT DEFAULT NULL,
+    questionIndex INT(11) DEFAULT NULL,
+    questionName TEXT,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (moduleId) REFERENCES module(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    )
+  `;
+    return new Promise((resolve, reject) => {
+        db.query(sql, (err, result) => {
+            if (err) {
+                reject('Error creating mcquestions table: ' + err);
+            } else {
+                resolve('mcquestions table created successfully. ✅');
+            }
+        });
+    });
+};
+
+
+
+const createMcqAnswersTable = () => {
+    const sql = `
+    CREATE TABLE IF NOT EXISTS mcqanswers (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      mcqId INT DEFAULT NULL,
+      answer TEXT,
+      trueOrFalse BOOLEAN DEFAULT NULL,
+      createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (mcqId) REFERENCES mcquestions(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    )
+  `;
+    return new Promise((resolve, reject) => {
+        db.query(sql, (err, result) => {
+            if (err) {
+                reject('Error creating mcqanswers table: ' + err);
+            } else {
+                resolve('mcqanswers table created successfully. ✅');
+            }
+        });
+    });
+};
+
+
+
+
+
+const createTfQuestionsTable = () => {
+    const sql = `
+    CREATE TABLE IF NOT EXISTS tfquestions (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      moduleId INT DEFAULT NULL,
+      questionIndex INT DEFAULT NULL,
+      question TEXT,
+      trueOrFalse BOOLEAN DEFAULT NULL,
+      createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      FOREIGN KEY (moduleId) REFERENCES module(id)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    )
+  `;
+    return new Promise((resolve, reject) => {
+        db.query(sql, (err, result) => {
+            if (err) {
+                reject('Error creating tfquestions table: ' + err);
+            } else {
+                resolve('tfquestions table created successfully. ✅');
+            }
+        });
+    });
+};
+
+
+
 
 module.exports = {
-    createUsersTable
+    createUsersTable,
+    createModuleTable,
+    createMcQuestionsTable,
+    createMcqAnswersTable,
+    createTfQuestionsTable
 };
